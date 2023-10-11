ec17f703edcaf15a7fae0a9f4824a8cd6558b723
Lucas Bent <lucasowenbent@gmail.com>
2023-10-11 13:44:42 -0400

    stuff

--- fe/src/Home.js
+++ fe/src/Home.js
@@ -8,6 +8,10 @@ function Home() {
             <div id='top'>
                 <h1>bbhacks</h1>
                 <h2>Black Bear Hacks.</h2>
+                <div id="top-bottom"></div>
+            </div>
+            <div id='about'>
+                <h2>about</h2>
             </div>
         </>
     )
