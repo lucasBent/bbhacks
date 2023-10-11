a7744900d74e21d301eb0472ed6f0e5e426c1d0f
Lucas Bent <lucasowenbent@gmail.com>
2023-10-10 23:16:32 -0400

    fix responsiveness of menu

--- fe/src/components/Nav.js
+++ fe/src/components/Nav.js
@@ -1,22 +1,33 @@
 import './Nav.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useMediaQuery } from 'react-responsive'
 
 function Nav() {
+
+    const maxMobileWidth = 1224
 
     function toggleExpanded() {
         setExpanded(!expanded)
     }
 
     function onIsTabletOrMobileChange(value) {
+        console.log(value)
         const nav = document.getElementById('navbar')
         nav.style.setProperty("--dropdown-height", `calc(${nav.querySelector('a').offsetHeight * 6}px + 4rem)`)
         setMobile(value)
     }
 
     const [expanded, setExpanded] = useState(false)
-    const [mobile, setMobile] = useState(true)
-    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' }, undefined, onIsTabletOrMobileChange)
+
+    useEffect(() => {
+        if (window.innerWidth <= maxMobileWidth)
+            setMobile(true)
+        else
+            setMobile(false)
+    }, [])
+
+    const [mobile, setMobile] = useState(false)
+    const isTabletOrMobile = useMediaQuery({ query: `(max-width: ${maxMobileWidth}px)` }, undefined, onIsTabletOrMobileChange)
 
     return (
         <div id='navbar' className={expanded ? 'expanded' : 'contracted'}>
